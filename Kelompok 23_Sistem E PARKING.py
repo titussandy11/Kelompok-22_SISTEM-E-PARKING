cb2d15efd1f89b9643990e8bbe44fa30521f9e57
sekarzaneta <80144833+sekarzaneta@users.noreply.github.com>
2021-05-27 15:49:43 +0700

    Update Kelompok 23_Sistem E PARKING.py

--- Kelompok 23_Sistem E PARKING.py
+++ Kelompok 23_Sistem E PARKING.py
@@ -1,1 +1,16 @@
+import winsound
+import time
+from time import sleep
 
+
+print("==========================================================")
+print()
+print('E-PARKING')
+
+
+# FUNGSI PEMBATAS
+def space():
+    print()
+    print("==========================================================");
+    sleep(1)
+    print()
