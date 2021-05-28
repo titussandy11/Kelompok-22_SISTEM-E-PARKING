07a9edb532d60bd25e69a09ed1e652a5cc01e583
raysaarma <80144771+raysaarma@users.noreply.github.com>
2021-05-28 10:14:13 +0700

    Update Kelompok 23_Sistem E PARKING.py

--- Kelompok 23_Sistem E PARKING.py
+++ Kelompok 23_Sistem E PARKING.py
@@ -15,6 +15,7 @@ def space():
     sleep(1)
     print()
 
+    
 # LOADING
 def wait():
     print("[MOHON TUNGGU SEBENTAR]");
@@ -26,3 +27,24 @@ def wait():
     print(".");
     sleep(0.25)
     print()
+
+    
+# PENGATURAN AWAL PARKING SYSTEM
+slot_motor = 20
+slot_mobil = 30
+jam1_motor = 3600
+biaya1_motor = 2000
+jam2_motor = 3600
+biaya2_motor = 3000
+jam1_mobil = 3600
+biaya1_mobil = 5000
+jam2_mobil = 3600
+biaya2_mobil = 7000
+space()
+
+
+# MENUNJUKKAN KETERSEDIAAN SLOT PARKIR
+def slot_parkir(motor_count, mobil_count):
+    print("[SLOT PARKIR TERSEDIA]")
+    print("Mobil =", mobil_count)
+    print("Motor =", motor_count)
